a9ef30a9452e3ccd68fadc1b1563fa3e27941d74
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-02-05 16:38:22 -0400

    feat: first tests and tool funcs

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Counter = () => {
   return (
     <div data-test="component-counter">
-
+      <h1 data-test="counter-display">
+        The counter is currently
+      </h1>
+      <button data-test="increment-button">
+        Increment counter
+      </button>
     </div>
   )
 };
--- src/components/Counter.test.jsx
+++ src/components/Counter.test.jsx
@@ -6,18 +6,29 @@ import Counter from './Counter';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the Counter component 
+ */
+const setup = () => shallow(<Counter />);
+
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test="${value}"]`);
+
 test('renders without error', () => {
-  const wrapper = shallow(<Counter />);
-  const counterComponent = wrapper.find('[data-test="component-counter"]');
+  const wrapper = setup();
+  const counterComponent = findByTestAttr(wrapper, 'component-counter');
   expect(counterComponent.length).toBe(1);
 });
 
 test('renders a button', () => {
-
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at 0', () => {
