1fc8a320af9838b2e3af637fa8b365f0ae51dc1a
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-02-08 21:09:29 -0400

    feat: success reducer with tests

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,6 +1,14 @@
+import { actionTypes } from '../actions';
 
-const reducer = (state, action) => {
-  return null;
+const defaultState = false;
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
