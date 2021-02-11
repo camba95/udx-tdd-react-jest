b1b2e9d556045b1778d7fcd2427d5738a7fb5488
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-02-11 18:30:30 -0400

    feat: Input redux props tests

--- src/components/Jotto/Input.jsx
+++ src/components/Jotto/Input.jsx
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { guessWord } from '../../actions';
 
-class Input extends Component {
+export class Input extends Component {
+  state = {
+    currentGuess: ''
+  };
+
+  submitGuessedWord = (event) => {
+    event.preventDefault();
+    const guessedWord = this.state.currentGuess;
+    if (guessedWord) {
+      this.props.guessWord(guessedWord);
+      this.setState({ currentGuess: '' });
+    }
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      currentGuess: event.target.value
+    });
+  };
+
   render() {
     const { success } = this.props;
+    const { currentGuess } = this.state;
     let content = null;
     if (!success) {
       content = (
@@ -14,11 +34,14 @@ class Input extends Component {
             className="mb-2 mx-sm-3"
             placeholder="Enter guess"
             type="text"
+            value={currentGuess}
+            onChange={this.onInputChange}
           />
           <button
             data-test="submit-button"
             className="btn btn-primary mb-2"
             type="submit"
+            onClick={this.submitGuessedWord}
           >
             Submit
           </button>
--- src/components/Jotto/Input.test.jsx
+++ src/components/Jotto/Input.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Input from './Input';
+import Input, { Input as UnconnectedInput } from './Input';
 import { findByTestAttr, storeFactory } from '../../../test/testUtils';
 
 const setup = (initialState = {}) => {
@@ -80,5 +80,38 @@ describe('redux props', () => {
     const { guessWord: guessWordProp } = wrapper.instance().props;
 
     expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+
+describe('guessWord action creator call', () => {
+  let guessWordMock;
+  let wrapper;
+  const guessedWord = 'train';
+  beforeEach(() => {
+    guessWordMock = jest.fn();
+    const props = {
+      guessWord: guessWordMock,
+      success: false
+    };
+    wrapper = shallow(<UnconnectedInput {...props} />);
+    wrapper.setState({ currentGuess: guessedWord });
+    const button = findByTestAttr(wrapper, 'submit-button');
+    button.simulate('click', { preventDefault() { } });
+  });
+
+  test('calls guessWord when button is clicked', () => {
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+
+    expect(guessWordCallCount).toBe(1)
+  });
+
+  test('calls guessWord with input value as argument', () => {
+    const guessWordArg = guessWordMock.mock.calls[0][0];
+
+    expect(guessWordArg).toBe(guessedWord)
+  });
+
+  test('input box clears on submit', () => {
+    expect(wrapper.state('currentGuess')).toBe('');
   });
 });
