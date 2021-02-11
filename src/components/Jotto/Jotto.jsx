6706f0337d1c264ff1b75d293ccc56f9f9369d89
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-02-10 22:33:29 -0400

    feat: Jotto, use real props

--- src/components/Jotto/Jotto.jsx
+++ src/components/Jotto/Jotto.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
 import { getSecretWord } from '../../actions';
 
 class Jotto extends Component {
   render() {
+    const { success, guessedWords } = this.props;
     return (
       <div className="container" data-test="component-jotto">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[{
-          guessedWord: 'train',
-          letterMatchCount: 3
-        }]} />
+        <Congrats success={success} />
+        <Input />
+        <GuessedWords guessedWords={guessedWords} />
       </div>
     );
   }
