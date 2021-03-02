80d8afdbcd891ef08a8a0a039ef0129955d92631
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-01 20:41:10 -0400

    feat: basic project to test context/hooks

--- src/helpers/utils.js
+++
@@ -1,5 +0,0 @@
-export const getLetterMatchCount = (guessedWord, secretWord) => {
-  const secretSet = new Set(secretWord);
-  const guessedSet = new Set(guessedWord);
-  return [...secretSet].filter((letter) => guessedSet.has(letter)).length;
-};
