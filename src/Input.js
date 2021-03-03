7000b1bab00f56fc419a9f26a42979b254a85c41
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-02 21:33:19 -0400

    feat: test on change with hook

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 function Input() {
+  const [currentGuess, setCurrentGuess] = React.useState('');
   return (
     <div data-test="component-input">
-
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(event) => event.preventDefault()}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
 }
 
 export default Input;
