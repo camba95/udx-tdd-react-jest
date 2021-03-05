2bfea5759c7c0e7e807cfa2404e10e323e43d5df
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-04 22:28:57 -0400

    feat: secretWord on update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import hookActions from './actions/hookActions';
 import './App.css';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const initialState = { secretWord: null };
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const setSecretWord = (secretWord) => dispatch({
+    type: "setSecretWord",
+    payload: secretWord
+  });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord)
+  }, []);
   return (
     <div data-test="component-app">
-      
+
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils'
 import App from './App';
+import hookActions from './actions/hookactions';
+
+const mockGetSecretWord = jest.fn();
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+  return mount(<App />);
 }
 
 test('App renders without erros', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
