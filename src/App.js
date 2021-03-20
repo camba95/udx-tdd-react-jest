5d0709756cebc76d3686780f751d0926ad450434
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-20 17:08:02 -0400

    feat: language picker and congrats comps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import hookActions from './actions/hookActions';
 import Input from './Input';
+import LanguagePicker from './LanguagePicker';
+import languageContext from './contexts/languageContext'
 import './App.css';
 
 function reducer(state, action) {
   switch (action.type) {
     case "setSecretWord":
       return { ...state, secretWord: action.payload };
+    case "setLanguage":
+      return { ...state, language: action.payload };
     default:
       return state;
   }
 }
 
 function App() {
-  const initialState = { secretWord: null };
+  const initialState = { secretWord: null, language: 'en' };
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
   const setSecretWord = (secretWord) => dispatch({
     type: "setSecretWord",
     payload: secretWord
+  });
+
+  const setLanguage = (language) => dispatch({
+    type: "setLanguage",
+    payload: language
   });
 
   React.useEffect(() => {
@@ -37,7 +46,11 @@ function App() {
 
   return (
     <div className="container" data-test="component-app">
-      <Input secretWord={state.secretWord} />
+      <h1>Jotto</h1>
+      <languageContext.Provider value={state.language}>
+        <LanguagePicker setLanguage={setLanguage} />
+        <Input secretWord={state.secretWord} />
+      </languageContext.Provider>
     </div>
   )
 }
